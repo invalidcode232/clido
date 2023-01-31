3fa69cc93ce03175a920e56491fe1ea5ffeebbef
invalidcode232 <78639481+invalidcode232@users.noreply.github.com>
2023-01-31 18:05:40 +0700

    feat: add initial todo add

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 pub mod todo;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { todo: String },
+}
 
 fn main() {
-    let mut todo = todo::Todo {
+    let cli = Cli::parse();
+
+    let mut todo_client = todo::Todo {
         ..Default::default()
     };
 
-    todo.init();
-    todo.write("The quick brown fox jumps over the lazy dog");
+    todo_client.init();
+
+    match &cli.command {
+        Some(Commands::Add { todo }) => {
+            todo_client.write(todo);
+        }
+        None => (),
+    }
 }
