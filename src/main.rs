ae7fce2404ff3c2464e0099415dcb534350638b6
invalidcode232 <78639481+invalidcode232@users.noreply.github.com>
2023-01-31 18:22:18 +0700

    refactor: add comments in main

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,16 @@ enum Commands {
 }
 
 fn main() {
-    let cli = Cli::parse();
-
+    // Initialize our todo client with default values
+    // TODO: Replace with the real path name
     let mut todo_client = todo::Todo {
         ..Default::default()
     };
 
     todo_client.init();
+
+    // Initialize our Clap cli parser
+    let cli = Cli::parse();
 
     match &cli.command {
         Some(Commands::Add { todo }) => {
