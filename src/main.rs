7c86d72be9271fa79d1ea140d9c589519f325306
invalidcode232 <78639481+invalidcode232@users.noreply.github.com>
2023-02-11 08:03:41 +0700

    feat: use a fixed path for storing todos

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod todo;
+
 use clap::{Parser, Subcommand};
 use todo::client;
 
@@ -12,12 +13,17 @@ struct Cli {
 enum Commands {
     Add { todo: String },
     List {},
+    Done { index: i32, is_done: String },
 }
 
 fn main() {
     // Initialize our todo client with default values
-    // TODO: Replace with the real path name
+    let path = dirs::home_dir()
+        .unwrap()
+        .join(".local/share/clido/todo.csv");
+
     let mut todo_client = client::TodoClient {
+        path: path.as_path(),
         ..Default::default()
     };
 
@@ -32,6 +38,18 @@ fn main() {
         }
         Some(Commands::List {}) => {
             todo_client.list();
+        }
+        Some(Commands::Done { index, is_done }) => {
+            let mut is_done_bool = true;
+            match is_done.to_owned().as_str() {
+                "t" => is_done_bool = true,
+                "true" => is_done_bool = true,
+                "f" => is_done_bool = false,
+                "false" => is_done_bool = false,
+                _ => println!("ss"),
+            }
+
+            todo_client.set_done(index.to_owned(), is_done_bool)
         }
         None => (),
     }
